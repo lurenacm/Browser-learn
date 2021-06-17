52304a86b947d6ee61bc9fb0c8867db23c168655
“lurenacm” <18320398437@139.com>
2021-06-17 22:05:11 +0800

    简单版的节流防抖

--- 浏览器架构/节流防抖/.js
+++ 浏览器架构/节流防抖/.js
@@ -1,4 +1,3 @@
-
 // function handel() {
 //     console.log("ajax sended -- ok")
 // }
@@ -27,7 +26,71 @@
 //     }
 // }
 
+window.onscroll = throttle(handel, 500)
 
-function throttle() {
-    
-} 
+function handel(event, ...params) {
+    console.log('ok')
+    console.log(event, ...params)
+}
+
+// function throttle(callback, wait) {
+//     if(typeof callback !== "function") throw new TypeError("func must be a function")
+//     if(typeof wait =="undefined") wait = 500
+
+//     let timer = null
+//     let  previous = 0   // 用于记录上一次的时间
+//     return function proxy(event, ...params){
+//         let self = this
+//         let now = new Date()
+
+//         setTimeout(() => {
+//             callback.call(self, ...params)
+//         }, wait);
+//     }
+// } 
+
+
+function throttle(func, timeout) {
+    let flag = false
+    return function (...args) {
+        let self = this
+        if (flag) return
+        flag = true
+        setTimeout(() => {
+            flag = false
+            func.call(self, ...args)
+        }, timeout);
+    }
+}
+
+
+function throttle(fn, interval) {
+    let flag = true;
+    return function (...args) {
+        let context = this;
+        if (!flag) return;
+        flag = false;
+        setTimeout(() => {
+            fn.apply(context, args);
+            flag = true;
+        }, interval);
+    };
+};
+
+
+window.onscroll = throttle(handel, 500)
+
+
+// function 
+
+function debounce(func, timeout){
+    let timer = null
+    return function(...args) {
+        let self = this
+        clearTimeout(timer)
+        timer = setTimeout(() => {
+           func.call(self, ...args) 
+        }, timeout);
+    }    
+}
+
