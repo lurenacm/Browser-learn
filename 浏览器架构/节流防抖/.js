f09c4b9359f7caed4d1f51cdcd97f44f9293386c
“lurenacm” <18320398437@139.com>
2021-06-17 16:30:18 +0800

    add 节流和防抖

--- 浏览器架构/节流防抖/.js
+++ 浏览器架构/节流防抖/.js
@@ -1,28 +1,33 @@
 
-function debounce(callback, wait, immediate){
-    // 参数的处理，防止漏传，误传。
-    if(typeof callback !=="function") throw new TypeError("func must be an function")
-    if(typeof wait == "undefined") wait = 500
-    if(typeof wait === "boolean") {
-        immediate = wait
-        wait = 500
-    }
-    if(typeof immediate !=="boolean") immediate = false
+// function handel() {
+//     console.log("ajax sended -- ok")
+// }
 
-    // 返回真正执行的函数。和开胃菜一样的思路，需要定时器和标识
-    let timer = null
-    return function proxy(){
-        // 在规定时间内再次点击，那么我们清除上一次的定时器，使用新的定时器。
-        // 假设规定时间内被点击了100次，那么前面的99次都是无效的，最后一次才有效果。
-        clearTimeout(timer)
-        timer = setTimeout(() => {
-            callback()
-        }, wait)
-    }
-}
+// box.onclick = debounce(handel, 500, true)
 
-function handel() {
-    console.log("ajax sended -- ok")
-}
+// function debounce(callback, wait, immediate) {
+//     if (typeof callback !== "function") throw new TypeError("func must be an function")
+//     if (typeof wait == "undefined") wait = 500
+//     if (typeof wait === "boolean") {
+//         immediate = wait
+//         wait = 500
+//     }
+//     if (typeof immediate !== "boolean") immediate = false
 
-box.onclick = debounce(handel, 500, true)
+//     let timer = null
+//     return function proxy(...params) {
+//         let self = this
+//         let now = immediate && !timer;
+//         clearTimeout(timer)
+//         timer = setTimeout(() => {
+//             timer = null
+//             !immediate ? callback.call(self, ...params) : null
+//         }, wait)
+//         now ? callback.call(self, ...params) : null
+//     }
+// }
+
+
+function throttle() {
+    
+} 
